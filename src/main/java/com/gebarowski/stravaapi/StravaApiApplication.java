ed57c69d2548068ac8bb7642a4a9221c3c6e7a4e
Michal Gebarowski <michalgebarowski@rentalcars.com>
2022-05-06 14:12:25 +0100

    first commit

---
+++ src/main/java/com/gebarowski/stravaapi/StravaApiApplication.java
@@ -0,0 +1,13 @@
+package com.gebarowski.stravaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StravaApiApplication.class, args);
+    }
+
+}
